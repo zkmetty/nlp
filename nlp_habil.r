216bc126220e59c28ea832b5a3985dce7159005d
zkmetty <56066927+zkmetty@users.noreply.github.com>
2022-02-08 22:44:03 +0100

    Update nlp_habil.r

--- nlp_habil.r
+++ nlp_habil.r
@@ -1,20 +1,21 @@
 
-###Basic Word Embedding analysis tools
+###VEKTORTÉR MODELLEK ELEMZÉSI LEHETŐSÉGEI
+
+##Csomagok (hiányzó csomagok install.packages paranccsal tölthetőek le)
 
 library(dplyr)
 library(text2vec)
 library(igraph)
+library(ggrepel)
 
-#Download pre-trained vector models:
+#wiki-news-300d-1M.vec letöltése:
 #https://fasttext.cc/docs/en/english-vectors.html
 
-
-
-#Use the most common 200k words
+#Csak a leggyakoribb 200k szót töltjük be
 wiki <- readLines("wiki-news-300d-1M.vec", n=200000, skip=1)
 
 
-#Create the vector-space models
+#Vektortér modell előkészítése
 wiki <- wiki[2:length(wiki)]
 
 wiki_list <- strsplit(wiki[]," ")
@@ -30,43 +31,44 @@ rm(i)
 rm(wiki)
 rm(wiki_list)
 
-#What are the closest neighbours of sociology?
+#Milyen szavak vannak a legközelebb a szociológiához
 
 s.word <- wiki_vec["sociology",,drop = FALSE]
 dist.s <- sim2(x = wiki_vec, y = s.word, method = "cosine", norm = "l2")
 dist.s[order(dist.s, decreasing=T),][1:40]
 
-#Distance of political and sociology
+#Politológia és szociológia távolsága
 
 sim2(x = wiki_vec["political",,drop = FALSE], y = wiki_vec["sociology",,drop = FALSE], method = "cosine", norm = "l2")
 
-#Distance of political+science and sociology
+#political+science és sociology távolsága
 
 sim2(x = wiki_vec["political",,drop = FALSE]+wiki_vec["science",,drop = FALSE], y = wiki_vec["sociology",,drop = FALSE], method = "cosine", norm = "l2")
+
+##Vizsgált tudományok kiválasztása
 
 szavak <- c("sociology","psychology","economics","philosophy","linguistics","biology","physics","mathematics","engineering","chemistry")
 szavak_vec <- wiki_vec[szavak,]
 
 koz.matrix <- sim2(x =szavak_vec, y = szavak_vec, method = "cosine", norm = "l2")
 
+##Klaszteres megközelítés
+
 
 vec.clust <- hclust(as.dist(1-koz.matrix), "ward.D")
 plot(vec.clust)
 
 
-#Network
+#Network megközelítés
 tr=0.6
 dist.matrix2 <- koz.matrix
 dist.matrix2[which(koz.matrix<tr)] <- 0 
 dist.matrix2[which(koz.matrix>=tr)] <- 1
 
-
 dist.matrix.graph <- graph_from_adjacency_matrix(dist.matrix2, "upper", diag=F)
 
-#Megcsináljuk azt, hogy két csoportra vágjuk szét a klaszterezést, és a kisebb csoportot bekaikázzuk
+#Megcsináljuk azt, hogy két csoportra vágjuk szét a klaszterezést, és a kisebb csoportot bekarikázzuk
 clust.group <- cutree(vec.clust, k = 2)
-
-
 
 plot(dist.matrix.graph, vertex.shape="none", vertex.label.font=2, 
      vertex.label.color=c(rep("black",length(V(dist.matrix.graph)))), 
@@ -96,13 +98,11 @@ ggplot(df_sne, aes(x=dim1, y= dim2, label = terulet)) +
   geom_text_repel(label=df_sne$terulet, size=7) +
   theme(text = element_text(size=20, colour = "black"), plot.title = element_text(size = 30, face = "bold"), legend.position="none")
 
-
-
-
-##
+##Konfirmatív modellek
 szavak2 <- c("sociologist","psychologist","economist","philosopher","linguist","biologist","physicist","mathematician","engineer","chemist")
 szavak_vec2 <- wiki_vec[szavak2,]
 
+##Tudományterületek távolsága "social" és "technical" szavaktól
 
 d1 <- round(sim2(x = wiki_vec["social",,drop = FALSE], y = szavak_vec, norm = "l2"),2)
 
@@ -112,27 +112,25 @@ d2 <- round(sim2(x = wiki_vec["technical",,drop = FALSE], y = szavak_vec, norm =
 df <- as.data.frame(cbind(szavak, t(d1),t(d2)))
 
 colnames(df) <- c("terulet","soft","hard")
-
-
-
-#require(ggrepel)
+df$soft <- as.numeric(df$soft)
+df$hard <- as.numeric(df$hard)
 
 ggplot(df, aes(x=hard, y= soft, label = terulet)) +
   geom_point(size=3) + 
   #geom_text(size=7, colour="darkblue") +
   xlab("technical") + ylab("social") + 
-#  xlim(0.3,0.6) +
-#  ylim(0.3,0.6) +
+  xlim(0.35,0.65) +
+  ylim(0.35,0.65) +
   theme_bw() +
   geom_text_repel(label=df$terulet, size=7) +
   theme(text = element_text(size=20, colour = "black"), plot.title = element_text(size = 30, face = "bold"), legend.position="none")
 
+##Tudományterületek távolsága "he" és "she" szavaktól
 
 f1 <- round(sim2(x = wiki_vec["he",,drop = FALSE] + wiki_vec["man",,drop = FALSE], y = szavak_vec2, norm = "l2"),2)
 f2 <- round(sim2(x = wiki_vec["she",,drop = FALSE] + wiki_vec["woman",,drop = FALSE], y = szavak_vec2, norm = "l2"),2)
 
 df2 <- as.data.frame(bind_cols(szavak2,  as.numeric(f1),  as.numeric(f2)))
-
 
 colnames(df2) <- c("szakma","he","she")
 
@@ -148,7 +146,7 @@ ggplot(df2, aes(x=he, y= she, label = szakma)) +
   theme(text = element_text(size=20, colour = "black"), plot.title = element_text(size = 30, face = "bold"), legend.position="none")
 
 
-##Korrupció
+##Országok távolsága a korrupció szótól
 
 
 s.word <- wiki_vec["corruption",,drop = FALSE]
@@ -180,6 +178,8 @@ tci <- c(75,81,45,70,50,48,57,84,88,82,37,41,29,48,50)
 
 
 cor(dist.s, tci)
+
+##Korrupció komplex mérése
 
 cor.words <- c("corruption","cronyism","malfeasance", "venality","graft","nepotism","bribery")
 
